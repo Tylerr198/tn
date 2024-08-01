7a69aa7f66c7bc8f8fa410154d35abdebaa0acd1
tyler <t198@bu.edu>
2024-08-01 12:00:44 -0400

    add initial website format

--- main.js
+++ main.js
@@ -1,26 +1,17 @@
-import * as THREE from 'three';
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
 
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0);
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
 
-const scene = new THREE.Scene();
-
-//create a blue LineBasicMaterial
-const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-
-const points = [];
-points.push( new THREE.Vector3( - 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
-
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-const line = new THREE.Line( geometry, material );
-
-scene.add( line );
-renderer.render( scene, camera );
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.scrollY >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
